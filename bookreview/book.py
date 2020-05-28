7ed53fdcfd14b5f05deb604f1a70c18bf41758ee
qweq23 <33680108+qweq23@users.noreply.github.com>
2020-05-28 15:38:53 +0900

    book.py 추가

---
+++ bookreview/book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, link, image, author, price, discount, publisher, pubdate, isbn, description):
+        self.title = title
+        self.link = link
+        self.image = image
+        self.author = author
+        self.price = price
+        self.discount = discount
+        self.publisher = publisher
+        self.pubdate = pubdate
+        self.isbn = isbn
+        self.description = description
