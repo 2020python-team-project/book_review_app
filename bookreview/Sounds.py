bfcb433df6bb7e87c0e6e2e7a008bf01a164d1a3
qweq23 <33680108+qweq23@users.noreply.github.com>
2020-06-23 02:35:22 +0900

    배포판!!! 마지막!!

--- bookreview/Sounds.py
+++ bookreview/Sounds.py
@@ -1,28 +1,28 @@
 from winsound import *
 
 def 뽁():
-    PlaySound('Resource/bgm/뽁.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/bbock.wav', SND_FILENAME)
 
 def 샤라락():
-    PlaySound('Resource/bgm/샤라락.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/shararack.wav', SND_FILENAME)
 
 def 따라란():
-    PlaySound('Resource/bgm/따라란.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/dararan.wav', SND_FILENAME)
 
 def 띠딩():
-    PlaySound('Resource/bgm/띠딩.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/didding.wav', SND_FILENAME)
 
 def 뿅():
-    PlaySound('Resource/bgm/뿅.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/bbyong.wav', SND_FILENAME)
 
 def 보석():
-    PlaySound('Resource/bgm/보석이빛날때.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/jewellery.wav', SND_FILENAME)
 
 def 띠리리링():
-    PlaySound('Resource/bgm/띠리리링.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/diririring.wav', SND_FILENAME)
 
 def 댐():
     PlaySound('Resource/bgm/댐.wav', SND_FILENAME)
 
 def 띵():
-    PlaySound('Resource/bgm/띵.wav', SND_FILENAME)
+    PlaySound('Resource/bgm/dding.wav', SND_FILENAME)
